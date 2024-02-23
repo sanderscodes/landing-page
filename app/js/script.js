417dde95ad11faefcc0be183cea8b6540d870c35
sanderscodes <sandzman7@gmail.com>
2024-02-23 02:52:51 -0500

    added fade in and out to overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,17 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 btnHamburger.addEventListener('click', function(){
 console.log('click hamburger');
-if(btnHamburger.classList.contains('open')){
-    btnHamburger.classList.remove('open');
+
+if(header.classList.contains('open')){ // close hamburger menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
 }
-else {
-    btnHamburger.classList.add('open');
+else { // open hamburger menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in')
 }
 });
